a6112b9f826993e206a13f09309061efbecf2fbb
Alvaro Garcia <derecerca@gmail.com>
2017-05-30 07:14:07 +0200

    Prefer finding the self-study by sorting

--- last_self-study.sh
+++ last_self-study.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-last_file=`ls -t content/blog/*markdown|grep self|grep study|head -1`
+last_file=`ls content/blog/*{md,markdown}|grep self|grep study|sort -rn|head -1`
 
 echo $last_file
 
