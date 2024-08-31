3c51656be77ce904e4ae7cccad675132eba14151
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-08-31 18:19:26 +0400

    [BuildingPrivateTools] brainstorm about ideas

---
+++ content/articles/2024-08-31-building-private-tools-open.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1725621267723" ID="ID_1589599453" MODIFIED="1725621267723" TEXT="New Mindmap">
+<node CREATED="1725621278341" ID="ID_1533822967" MODIFIED="1725621399558" POSITION="right" TEXT="Separate the public and private infromation">
+<node CREATED="1725621400027" ID="ID_1648780728" MODIFIED="1725621402545" TEXT="Data"/>
+<node CREATED="1725621403238" ID="ID_1478720131" MODIFIED="1725621405945" TEXT="Behavior"/>
+</node>
+</node>
+</map>
